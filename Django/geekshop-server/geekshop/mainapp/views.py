83e0593c2afe06a9c8050353f9437a700f439242
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2020-12-07 16:47:01 +0400

    Hw1

--- Django/geekshop-server/geekshop/mainapp/views.py
+++ Django/geekshop-server/geekshop/mainapp/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'mainapp/index.html')
 
-
 def product(request):
       return render(request, 'mainapp/products.html')
 
