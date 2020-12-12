17a11d1e7a946993e54bb915d3767870c00c7547
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2020-12-12 18:24:36 +0400

    Set up all the transitions between pages

--- mainapp/views.py
+++ mainapp/views.py
@@ -10,4 +10,19 @@ def product(request):
 
 
 def test_context(request):
-    return render(request, 'mainapp/context.html')
+    context = {
+        'title': 'добро поЖаловать',
+        'username': 'Viktor Dobkin',
+        'products': [
+            {'name': 'Черное худи', 'price': '2 9990 рублей'},
+            {'name': 'Джинсы', 'price': '5 800 рублей'},
+        ],
+        'promotion': True,
+        'promotion_products': [
+            {'name': 'Туфли ', 'price': '9 800 рублей'},
+        ]
+    }
+
+    products = context['products']
+
+    return render(request, 'mainapp/context.html', context)
