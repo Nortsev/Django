b76a37d45af3fdfaedba832412e39e543203c683
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2020-12-13 00:28:22 +0400

    Set the title headers (title)

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'GeekShop'
+       }
+    return render(request, 'mainapp/index.html', context)
 
 
 def product(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'title': 'GeekShop - Каталог'
+    }
+    return render(request, 'mainapp/products.html', context)
 
 
 def test_context(request):
