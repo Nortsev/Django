33aa7165c92f2775fb0411e37c0367bc936c28ec
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2020-12-17 22:51:23 +0400

    Upload migation in project

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='users_avatar', blank=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
--- geekshop/settings.py
+++ geekshop/settings.py
@@ -121,3 +121,5 @@ STATICFILES_DIRS = (
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'authapp.User'
