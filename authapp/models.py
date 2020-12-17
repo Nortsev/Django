cce45817b91e5bb4fd794a90109b6849e2e59394
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2020-12-18 00:57:52 +0400

    Settings Login in form

--- authapp/models.py
+++ authapp/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatar', blank=True)
-    age = models.PositiveIntegerField(blank=True, null=True)
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.username
--- authapp/views.py
+++ authapp/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponsePermanentRedirect
+from django.contrib import auth
+from django.urls import reverse
+from authapp.forms import UserLoginForm
+
 
 def login(request):
-    return render(request,'authapp/login.html')
+    form = UserLoginForm(data=request.POST)
+    if request.method == 'POST' and form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponsePermanentRedirect(reverse('main'))
+
+    context = {'form': form}
+    return render(request, 'authapp/login.html', context)
