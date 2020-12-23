a1000c8d24a9b4cc1aacf15b9c4f1f8c41303472
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2020-12-23 23:56:40 +0400

    Add function basket

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp_views.index, name='main'),
     path('products/', include('mainapp.urls', namespace='products')),
-    path('auth/', include('authapp.urls')),
+    path('auth/', include('authapp.urls', namespace="auth")),
+    path('baskets/', include('basketapp.urls', namespace="baskets")),
 
 ]
 
