19ca042e151f89e8c165be930d28a8e049781908
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2020-12-30 20:58:20 +0400

    Add function paginator

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,6 +6,7 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', mainapp.products, name="products"),
     path('<int:category_id>/', mainapp.products, name='product'),
+    path('<int:page>/', mainapp.products, name='page'),
 ]
 
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,20 +1,28 @@
 from django.shortcuts import render
 from mainapp.models import ProductCategory, Product
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
     return render(request, 'mainapp/index.html')
 
 
-def products(request, category_id=None):
+def products(request,category_id=None, page =1 ):
     context = {
             'categories': ProductCategory.objects.all(),
     }
     if category_id:
         products = Product.objects.filter(category_id=category_id)
-        context.update({'products': products})
     else:
-        context.update({'products': Product.objects.all()})
+        products = Product.objects.all()
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context.update({'products': products_paginator})
 
     return render(request, 'mainapp/products.html', context=context)
 
