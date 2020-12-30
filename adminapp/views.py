3354f659a31bd1f11bea8af1c67ffd8c3966db28
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2020-12-31 00:24:26 +0400

    Add work in class apmiapp

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,55 +1,47 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from authapp.models import User
 from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
+from django.utils.decorators import method_decorator
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def index(request):
     return render(request, 'adminapp/index.html')
 
-
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users(request):
-    context = {
-        'users': User.objects.all(),
-    }
-    return render(request, 'adminapp/admin-users-read.html', context)
-
-
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegisterForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
-
-    else:
-        form = UserAdminRegisterForm()
-
-    context = {'form': form}
-    return render(request, 'adminapp/admin-users-create.html', context)
+class UserListView(ListView):
+    model = User
+    template_name = 'adminapp/admin-users-read.html'
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserListView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_update(request, user_id):
-    user = User.objects.get(id=user_id)
-    if request.method == "POST":
-        form = UserAdminProfileForm(data=request.POST, files=request.FILES, instance=user)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
-    else:
-        form = UserAdminProfileForm(instance=user)
 
-    context = {
-        'form': form,
-        'user': user,
-    }
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'adminapp/admin-users-create.html'
+    success_url = reverse_lazy('adminapp:admin_users')
+    form_class = UserAdminRegisterForm
 
-    return render(request, 'adminapp/admin-users-update-detele.html', context)
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserCreateView, self).dispatch(request, *args, **kwargs)
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = 'adminapp/admin-users-update-detele.html'
+    success_url = reverse_lazy('adminapp:admin_users')
+    form_class = UserAdminProfileForm
+
+    @method_decorator(user_passes_test(lambda u: u.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserUpdateView, self).dispatch(request, *args, **kwargs)
+
 
 
 @user_passes_test(lambda u: u.is_superuser)
